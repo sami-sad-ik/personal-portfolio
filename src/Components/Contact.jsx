dc50ad9aff371d0945236648c6834ac53e6a6534
sami-sad-ik <sadiksami530@gmail.com>
2025-11-06 06:43:22 +0600

    contact section responsiveness fixed

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -44,8 +44,8 @@ const Contact = () => {
   return (
     <section id="contact" className="mt-10 w-full bg-base-300 py-12">
       <h2 className="py-6 text-4xl text-center font-bold">Contact</h2>
-      <div className="w-9/12 mx-auto flex justify-between items-center">
-        <div className="w-1/2 mx-auto flex flex-col gap-3">
+      <div className="w-9/12 mx-auto flex flex-col md:flex-row justify-between items-center gap-5">
+        <div className="md:w-1/2 w-full mx-auto flex flex-col gap-3">
           <div className="flex flex-col gap-2">
             <a
               href="tel:+8801632350530"
@@ -79,8 +79,8 @@ const Contact = () => {
             </a>
           </div>
         </div>
-        <form onSubmit={handleSubmit} className="w-1/2">
-          <label class="sr-only" for="name">
+        <form onSubmit={handleSubmit} className="md:w-1/2 w-full">
+          <label className="sr-only" for="name">
             Name
           </label>
           <input
@@ -88,10 +88,10 @@ const Contact = () => {
             name="name"
             placeholder="Name"
             required
-            class="w-full mb-4 px-4 py-2 rounded-lg border placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-400 transition"
+            className="w-full mb-4 px-4 py-2 rounded-lg border placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-400 transition"
           />
 
-          <label class="sr-only" for="email">
+          <label className="sr-only" for="email">
             Email
           </label>
           <input
@@ -99,23 +99,23 @@ const Contact = () => {
             name="email"
             placeholder="Email"
             required
-            class="w-full mb-4 px-4 py-2 rounded-lg border placeholder-gray-400  focus:outline-none focus:ring-2 focus:ring-blue-400 transition"
+            className="w-full mb-4 px-4 py-2 rounded-lg border placeholder-gray-400  focus:outline-none focus:ring-2 focus:ring-blue-400 transition"
           />
 
-          <label class="sr-only" for="message">
+          <label className="sr-only" for="message">
             Message
           </label>
           <textarea
             rows="4"
             name="message"
             placeholder="Write your message..."
-            class="w-full mb-4 px-4 py-2 rounded-lg border placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-400 transition"></textarea>
+            className="w-full mb-4 px-4 py-2 rounded-lg border placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-400 transition"></textarea>
 
-          <div class="flex items-center gap-3">
+          <div className="flex items-center gap-3">
             <button
               type="submit"
               disabled={loading}
-              class="disabled:cursor-not-allowed px-5 py-2 rounded-lg bg-amber-500 font-semibold hover:bg-amber-600 transition-all">
+              className="disabled:cursor-not-allowed px-5 py-2 rounded-lg bg-amber-500 font-semibold hover:bg-amber-600 transition-all">
               Send Message
             </button>
           </div>
